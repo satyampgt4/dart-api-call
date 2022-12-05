83611d2f81bdf8df4e5e2790d3ada46e7d4c81c0
Satyam Dengre <59261812+satyampgt4@users.noreply.github.com>
2022-12-05 11:24:02 +0530

    Create SendOTP.dart

---
+++ SendOTP.dart
@@ -0,0 +1,18 @@
+import 'dart:html';
+
+void main() {
+  const data =
+      '{\n  \"Param1\": \"value1\",\n  \"Param2\": \"value2\",\n  \"Param3\": \"value3\"\n}';
+  var request = new HttpRequest();
+  request.withCredentials = true;
+
+  request.open(
+      'GET', 'https://api.msg91.com/api/v5/otp?template_id=&mobile=&authkey=');
+  request.setRequestHeader('Content-Type', 'application/JSON');
+
+  request.onLoad.listen(
+      (event) => print('Request complete ${event.target?.reponseText}'));
+  request.send();
+
+  request.send(data);
+}
